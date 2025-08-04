8aa0e3d309df13160251f515dcc4fe26322599a0
alecq031723 <144410466+alecq031723@users.noreply.github.com>
2025-08-04 17:49:44 -0400

    Finished home section

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ window.onscroll = () => {
   navbar.classList.remove("active");
   contactInfo.classList.remove("active");
 };
+
+var swiper = new Swiper(".home-slider", {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
